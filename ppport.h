cc7e28eb41fba3d9443d3f97ddb9512d4f4d26be
richdawe <richdawe>
2007-05-06 10:00:44 +0000

    Fix compile warning

--- ppport.h
+++ ppport.h
@@ -221,14 +221,16 @@ __DATA__
 /* Replace: 0 */
 #endif
 
-#ifdef HASATTRIBUTE
-#  if (defined(__GNUC__) && defined(__cplusplus)) || defined(__INTEL_COMPILER)
-#    define PERL_UNUSED_DECL
+#if defined(HASATTRIBUTE)
+#  if !defined(PERL_UNUSED_DECL)
+#    if (defined(__GNUC__) && defined(__cplusplus)) || defined(__INTEL_COMPILER)
+#      define PERL_UNUSED_DECL
+#    else
+#      define PERL_UNUSED_DECL __attribute__((unused))
+#    endif
 #  else
-#    define PERL_UNUSED_DECL __attribute__((unused))
+#    define PERL_UNUSED_DECL
 #  endif
-#else
-#  define PERL_UNUSED_DECL
 #endif
 
 #ifndef dNOOP
