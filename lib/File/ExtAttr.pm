93f19a86d4835dd907e262ca773dfb55855c464b
richdawe <richdawe>
2007-05-06 10:02:13 +0000

    Bump version to 1.04

--- lib/File/ExtAttr.pm
+++ lib/File/ExtAttr.pm
@@ -180,7 +180,7 @@ our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 our @EXPORT = qw(
 );
 
-our $VERSION = '1.03';
+our $VERSION = '1.04';
 
 #this is used by getxattr(), needs documentation
 $File::ExtAttr::MAX_INITIAL_VALUELEN = 255;
